2cd832dd36466d1e14e0ebb0b3cf2c0ad1c681cb
Alexander Oliveira <alexander.oliveira@take.net>
2022-09-19 17:12:35 -0300

    linkbutton component

---
+++ src/components/layout/LinkButton.js
@@ -0,0 +1,12 @@
+import styles from './LinkButton.module.css';
+import { NavLink} from 'react-router-dom';
+
+function LinkButton({to, text}) {
+    return (
+        <NavLink className={styles.btn} to={to}>
+            {text}
+        </NavLink>
+    )
+}
+
+export default LinkButton
