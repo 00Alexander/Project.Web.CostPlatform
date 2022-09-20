5655e11db7121797b451e681bedcce38d2c900b7
Alexander Oliveira <alexander.oliveira@take.net>
2022-09-20 14:50:43 -0300

    loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom"
 import Message from "../layout/Message"
 import Container from '../layout/Container';
-
+import Loading from '../layout/Loading';
 import styles from './Project.module.css'
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
@@ -9,40 +9,51 @@ import { useState, useEffect } from "react";
 
 function Projects() {
 
-    const [projects, setProjects] = useState([]) 
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
-    let message =  ''
-    if(location.state){
+    let message = ''
+    if (location.state) {
         message = location.state.message
-        
+
     }
 
-    useEffect(() =>{
-        fetch("http://localhost:5000/projects",{
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) =>{
-            console.log(data)
-            setProjects(data)
-        })
-        .catch((err) => console.log(err))
-    },[])
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:5000/projects", {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 1000)
+    }, [])
 
 
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
-            <h1>Meus Projetos</h1>
-            <LinkButton to="/newproject" text="Criar Projeto"/>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
-            {message && <Message type="success" msg={message}/>}
+            {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                {projects.length > 0 && projects.map((data) => <ProjectCard name={data.name} budget={data.budget} category={data.category.name}/>)}
+                {projects.length > 0 && projects.map((data) => (
+                    <ProjectCard name={data.name} budget={data.budget} category={data.category.name} />
+
+                ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
