4fde1bc07125a4dca6658e8de0db74a33867d6f6
Alexander Oliveira <alexander.oliveira@take.net>
2022-09-20 10:18:27 -0300

    message e apis

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,13 +1,14 @@
 import styles from './Select.module.css';
 
-function Select({ text, name, optins, handleOnChage, value }) {
+function Select({ text, name, options, handleOnChage, value }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
-            <select
-                name={name}
-                id={name}>
-                    <optins>Selecione uma opção</optins>
+            <select name={name} id={name} onChange={handleOnChage} value={value || ''}>
+                    <option>Selecione uma opção</option>
+                    {options.map((option) => (
+                        <option value={option.id} key={option.id}>{option.name}</option>
+                    ))}
             </select>
         </div>
     )
